5cf4c2a07e868b52e15f2d7b50d7ba422a0c714c
Isaque Santos <isaque@MacBook-Pro-de-Isaque.local>
2025-09-04 22:38:47 -0300

    chore(init): adiciona skeleton do Spring Boot (pom e src)

---
+++ src/main/java/br/com/itau/pixkeys/PixKeysApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.pixkeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixKeysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixKeysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itau/pixkeys/PixKeysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.pixkeys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixKeysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
