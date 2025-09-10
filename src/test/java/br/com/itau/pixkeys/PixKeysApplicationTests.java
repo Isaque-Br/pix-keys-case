e9554e19ceb4aea178cd1fb72cd0df73838dea10
Isaque Santos <izakesz@gmail.com>
2025-09-10 15:40:03 -0300

    test(smoke): evita Mongo no contextLoads excluindo autoconfig e mockando PixKeyRepository

--- src/test/java/br/com/itau/pixkeys/PixKeysApplicationTests.java
+++ src/test/java/br/com/itau/pixkeys/PixKeysApplicationTests.java
@@ -1,10 +1,22 @@
 package br.com.itau.pixkeys;
 
+import br.com.itau.pixkeys.infrastructure.repository.PixKeyRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.NONE,
+		properties = {
+				"spring.autoconfigure.exclude=" +
+						"org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration," +
+						"org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration"
+		}
+)
 class PixKeysApplicationTests {
+
+	// Mocka o repo para o PixKeyService poder ser criado sem Mongo
+	@MockitoBean
+	PixKeyRepository repo;
 
 	@Test
 	void contextLoads() {
